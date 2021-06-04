9e55076923c3df7161991953989959f349362240
Chris Gutierrez <chris.n.gutierrez@gmail.com>
2021-06-04 13:05:05 -0700

    home and friends pages

---
+++ src/pages/friends.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Friends extends React.Component {
+  render() {
+    return (
+      <>
+        <div>
+          Friends
+        </div>
+      </>
+    )
+  }
+}
+
+export default Friends;
