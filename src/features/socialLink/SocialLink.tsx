fef715aee02075435646a123b6c0eb4887d12106
cgutierrez7 <chris.n.gutierrez@gmail.com>
2021-06-21 23:56:58 -0700

    initial SocialLink component work

---
+++ src/features/socialLink/SocialLink.tsx
@@ -0,0 +1,12 @@
+export function SocialLink() {
+  const address: string = "someURL"
+  return (
+    <>
+      <div>
+        <a href={address}>
+          Link to socials here
+        </a>
+      </div>
+    </>
+  )
+}
--- src/pages/friends.tsx
+++ src/pages/friends.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
+import { SocialLink } from '../features/socialLink/SocialLink';
 
 class Friends extends React.Component {
   render() {
+    const links: string[] = ["link1","link2","link3"]
     return (
       <>
         <div>
           Friends
         </div>
+        {links.map(link => {
+          return <SocialLink key={link}/>
+        })}
       </>
     )
   }
