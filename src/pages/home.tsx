60c0f5d9b41b652abebc7e000839c3f51eb06357
cgutierrez7 <chris.n.gutierrez@gmail.com>
2021-08-01 21:41:14 -0700

    placeholder home text and formatting

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -6,8 +6,16 @@ class Home extends React.Component {
     return (
     <>
       <div>
-        Hello
+        <h1>Hello</h1>
       </div>
+        <div>
+          <p style={{width:"65%", margin:"1em auto 0 auto"}}>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Urna porttitor rhoncus dolor purus non enim. Ullamcorper a lacus vestibulum sed arcu non odio euismod lacinia. Morbi blandit cursus risus at ultrices mi tempus. Risus sed vulputate odio ut enim blandit volutpat maecenas. Etiam erat velit scelerisque in dictum non. In tellus integer feugiat scelerisque. Turpis egestas integer eget aliquet nibh. Elementum nibh tellus molestie nunc non blandit massa enim. Eget egestas purus viverra accumsan in nisl. Bibendum neque egestas congue quisque. Eu augue ut lectus arcu bibendum at. Proin libero nunc consequat interdum varius sit amet mattis. Sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque. Tristique senectus et netus et malesuada fames. Risus in hendrerit gravida rutrum. In arcu cursus euismod quis.
+          </p>
+          <p style={{width:"65%", margin:"1em auto 0 auto"}}>
+            Suspendisse potenti nullam ac tortor. Nisl nunc mi ipsum faucibus. Semper auctor neque vitae tempus quam pellentesque nec. Nulla aliquet enim tortor at auctor urna. In est ante in nibh mauris cursus mattis molestie a. Quis ipsum suspendisse ultrices gravida dictum fusce ut. Et netus et malesuada fames ac turpis egestas. Adipiscing diam donec adipiscing tristique risus nec. Scelerisque in dictum non consectetur a. Vel fringilla est ullamcorper eget. Id diam vel quam elementum pulvinar. Nunc mattis enim ut tellus elementum sagittis vitae et. Vulputate enim nulla aliquet porttitor. Sagittis eu volutpat odio facilisis mauris sit amet massa vitae. Placerat orci nulla pellentesque dignissim. Orci phasellus egestas tellus rutrum tellus.
+          </p>
+        </div>
     </>
     )
   }
